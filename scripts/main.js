8f34976fb7f342fba86a53347c0df6a6ef55ea55
Mehmet Şahin <mehmet.sahin9910@gmail.com>
2022-11-07 22:17:50 +0300

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -4,44 +4,42 @@ let countries = [];
 const loadCountries = async () =>{
     const response = await fetch("data/countries.json");
     countries = await response.json();
-    showCountries(countries);
 }
 
+// const cardElement = document.getElementById("country-card");
 
-const cardElement = document.getElementById("country-card");
+// const showCountries = (countriesParam) =>{
+//     const countryCard = countriesParam
+//                     .map((country) =>{
+//                         return `
+//                             <div class="card">
+//                                 <div class="image">
+//                                     <img style=" width=250px; height=125px;"id="country-image" src="${country.flag}" alt="">
+//                                 </div>
+//                                 <div class="info">
+//                                     <b>Name:</b> ${country.name} <br>
+//                                     <b>Capital:</b> ${country.capital} <br>
+//                                     <b>Population:</b> ${country.population} <br>
+//                                     <b>Region:</b> ${country.region} 
+//                                 </div>
+//                             </div>
+//                         `;
+//                     });
+//                     cardElement.innerHTML = countryCard;
+// }
 
-const showCountries = (countriesParam) =>{
-    const countryCard = countriesParam
-                    .map((country) =>{
-                        return `
-                            <div class="card">
-                                <div class="image">
-                                    <img style=" width=250px; height=125px;"id="country-image" src="${country.flag}" alt="">
-                                </div>
-                                <div class="info">
-                                    <b>Name:</b> ${country.name} <br>
-                                    <b>Capital:</b> ${country.capital} <br>
-                                    <b>Population:</b> ${country.population} <br>
-                                    <b>Region:</b> ${country.region} 
-                                </div>
-                            </div>
-                        `;
-                    });
-                    cardElement.innerHTML = countryCard;
-}
-
-loadCountries();
+// loadCountries();
 
 
-let countryName = document.getElementById("input");
+// let countryName = document.getElementById("input");
 
-countryName.addEventListener("input", (e) =>{
-    const searchCountryName = e.target.value.toLowerCase();
-    const filteredCountries = countries.filter((country) => {
-        return (country.name.toLowerCase().includes(searchCountryName));
-    });
-    showCountries(filteredCountries);
-});
+// countryName.addEventListener("input", (e) =>{
+//     const searchCountryName = e.target.value.toLowerCase();
+//     const filteredCountries = countries.filter((country) => {
+//         return (country.name.toLowerCase().includes(searchCountryName));
+//     });
+//     showCountries(filteredCountries);
+// });
 
 
 
